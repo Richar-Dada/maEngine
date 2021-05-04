8cef349de30c92020c884b7c008eea38a47cf549
Richar-Dada <631151149@qq.com>
2021-05-04 21:22:20 +0800

    init project

---
+++ dist/bundle.js
@@ -0,0 +1,12 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+var __webpack_exports__ = {};
+/*!*********************!*\
+  !*** ./src/main.ts ***!
+  \*********************/
+
+console.log('hello');
+
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9tYUVuZ2luZS8uL3NyYy9tYWluLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7Ozs7OztBQUFhO0FBQ2IiLCJmaWxlIjoiLi9idW5kbGUuanMiLCJzb3VyY2VzQ29udGVudCI6WyJcInVzZSBzdHJpY3RcIjtcbmNvbnNvbGUubG9nKCdoZWxsbycpO1xuIl0sInNvdXJjZVJvb3QiOiIifQ==
